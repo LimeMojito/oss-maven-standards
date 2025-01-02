8b94326013e30e3770ed3350bb05f257cac6d28e
lachlan <lachlan.odonnell@limemojito.com>
2025-01-02 11:23:55 +1100

    Copyright Update

--- utilities/aws-utilities/lambda-utilities/src/main/java/com/limemojito/aws/lambda/SnapStartOptimizer.java
+++ utilities/aws-utilities/lambda-utilities/src/main/java/com/limemojito/aws/lambda/SnapStartOptimizer.java
@@ -1,9 +1,18 @@
 /*
- * Copyright (c) Lime Mojito Pty Ltd 2011-2024
+ * Copyright 2011-2025 Lime Mojito Pty Ltd
  *
- * Except as otherwise permitted by the Copyright Act 1967 (Cth) (as amended from time to time) and/or any other
- * applicable copyright legislation, the material may not be reproduced in any format and in any way whatsoever
- * without the prior written consent of the copyright owner.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  */
 
 package com.limemojito.aws.lambda;
